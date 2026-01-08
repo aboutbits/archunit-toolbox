bb3d07792359be4938e8b05453be54cabdbf04b8
SirCotare <andreas.hufler@aboutbits.it>
2026-01-08 11:22:09 +0100

    project setup & base rules (#1)
    * project setup
    
    * add base rules
    
    * fix issues

---
+++ src/test/java/it/aboutbits/archunit/toolbox/ArchitectureTest.java
@@ -0,0 +1,14 @@
+package it.aboutbits.archunit.toolbox;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.CacheMode;
+import org.jspecify.annotations.NullMarked;
+
+@AnalyzeClasses(
+        packages = ArchitectureTest.PACKAGE,
+        cacheMode = CacheMode.PER_CLASS
+)
+@NullMarked
+class ArchitectureTest extends ArchitectureTestBase {
+    static final String PACKAGE = "it.aboutbits.archunit.toolbox";
+}
